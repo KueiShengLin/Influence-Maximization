61e36e8606618b83870bcf0502040c45b519255a
Tim Lin <tarry21304@gmail.com>
2018-06-04 00:31:40 +0800

    general greedy

--- greedyAlgo.py
+++ greedyAlgo.py
@@ -14,25 +14,24 @@ def general_greedy(g, k=5, p=0.1, R=20):
     :return: Seed set S
     """
     IC = IndependentCascade(g, p)
-
     S = []
 
     for i in range(k):
-        sv = {}
+        sv = {}  # the score for node not in S
 
         for v in g.nodes():
             if v not in S:
                 sv[v] = 0
                 union = copy.copy(S)
-                union.append(v)
+                union.append(v)  # union = {S U v}
 
-                for round in range(R):
+                for round_time in range(R):
                     sv[v] += len(IC.run(union))
 
                 sv[v] = sv[v] / R
 
         sorted_sv = sorted(sv.items(), key=lambda x: x[1], reverse=True)  # x = sv, x[0] = sv.keys(), x[1] = sv.values()
-        S.append(sorted_sv[0][0])
+        S.append(sorted_sv[0][0])   # S U (node which have highest score)
 
     return S
 
@@ -42,7 +41,7 @@ if __name__ == '__main__':
     graph_data.columns = ['node_1', 'node_2']
     G = nx.from_pandas_edgelist(graph_data, target='node_1', source='node_2')
 
-    seed_set = general_greedy(G, 2, 0.1, R=1000)
+    seed_set = general_greedy(G, 1, 0.1, R=2000)
     print(seed_set)
 
 #
